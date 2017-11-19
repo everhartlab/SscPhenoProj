d1483124c70e9bb62696c68e278c828f504b2985
sporangia <everhart@unl.edu>
2017-11-19 15:53:57 -0600

    create side-by-side figure

--- Analysis.R
+++ Analysis.R
@@ -11,6 +11,7 @@ library(ggplot2)
 library(PMCMR)
 library(plotrix)
 library(Hmisc)
+library(grid)
 ################# Reading raw data from Excel file ############################
 ssc_summary <- read_excel("Brazilian agressiveness_raw_data-final2.xlsx", sheet="Summary", na = c("", "NA"), col_names = FALSE)
 #%>% write_csv("Results-single.csv")
@@ -19,12 +20,13 @@ ssc_summary
 #### Evaluations of isolates:
 aproj <- read_excel("Brazilian agressiveness_raw_data-final2.xlsx", sheet="A",na = c("", "NA"))
 bproj <- read_excel("Brazilian agressiveness_raw_data-final2.xlsx", sheet="B",na = c("", "NA"), range="A1:F385") #trim last col
+bproj$`8 dai (cm)` <- as.numeric(bproj$`8 dai (cm)`)
 cproj <- read_excel("Brazilian agressiveness_raw_data-final2.xlsx", sheet="C",na = c("", "NA"))
 dproj <- read_excel("Brazilian agressiveness_raw_data-final2.xlsx", sheet="D",na = c("", "NA"))
 #### Evaluations of cultivars:
 eproj <- read_excel("Brazilian agressiveness_raw_data-final2.xlsx", sheet="E",na = c("", "NA"))
 fproj <- read_excel("Brazilian agressiveness_raw_data-final2.xlsx", sheet="F",na = c("", "NA"))
-gproj <- read_excel("Brazilian agressiveness_raw_data-final2.xlsx", sheet="G",na = c("", "NA"))
+gproj <- read_excel("Brazilian agressiveness_raw_data-final2.xlsx", sheet="G",na = c("", "NA")) 
 hproj <- read_excel("Brazilian agressiveness_raw_data-final2.xlsx", sheet="H",na = c("", "NA"), range="A1:F323") #trim last three cols
 iproj <- read_excel("Brazilian agressiveness_raw_data-final2.xlsx", sheet="I",na = c("", "NA"), range="A1:D286") #trim last 4 cols
 
@@ -35,28 +37,59 @@ asum <- aproj %>% group_by(Isolate) %>% summarize(n = n(), mean = mean(Area), mi
 asum <- aproj %>% group_by(Isolate) %>% summarize(n = n(), mean = mean(Area), min = min(Area), max = max(Area), sd = sd(Area))
 bsum <- bproj %>% group_by(Isolate) %>% summarize(n = n(), mean = mean(`8 dai (cm)`), min = min(`8 dai (cm)`), max = max(`8 dai (cm)`), sd = sd(`8 dai (cm)`))
 csum <- cproj %>% group_by(Isolate) %>% summarize(n = n(), mean = mean(`48 horas`), min = min(`48 horas`), max = max(`48 horas`), sd = sd(`48 horas`))
+dsum <- dproj %>% group_by(Isolate) %>% summarize(n = n(), mean = mean(Score), min = min(Score), max = max(Score), sd = sd(Score))
 
 agg <- cbind(asum, rep("a", length(asum$sd)))
 bgg <- cbind(bsum, rep("b", length(bsum$sd)))
+cgg <- cbind(csum, rep("c", length(csum$mean)))
+dgg <- cbind(dsum, rep("d", length(dsum$mean)))
+
 colnames(agg)[7] <- "proj"
 colnames(bgg)[7] <- "proj"
-tot <- rbind(agg,bgg)
+colnames(cgg)[7] <- "proj"
+colnames(dgg)[7] <- "proj"
+dlb <- rbind(agg,cgg)
+st <- rbind(bgg,dgg)
 
-p2 <- asum %>%
-    ggplot(mapping=aes(x=1, y = mean)) +
-    geom_dotplot(binaxis = "y", stackdir = "center", fill=NA, binwidth = .4)
-p2
-## dotplot with mean point in red
-p2 + stat_summary(fun.y=mean, geom="point", shape=18,
-                 size=3, color="red")
-ggplot(asum, aes(x=1, y=mean)) + 
-  geom_violin(trim = FALSE)+
-  geom_dotplot(binaxis='y', stackdir='center')
-p2 + stat_summary(fun.data="mean_sdl", fun.args = list(mult=1), 
-                 geom="pointrange", color="red")
+p2 <- dlb %>%
+  ggplot(mapping=aes(x=proj, y = mean)) +
+  geom_jitter(width = .1, height = 0, shape=21, color="black", fill="orange", size=3.5, alpha=1/2) +
+  ###  change fill to be equal to the origin of the isolate ==== Brazil or USA
+  stat_summary(fun.y=mean, geom="point", shape=95, size=12, color="black") + 
+  ## there are several labeling features within labs() -- check help for more options
+  theme_minimal() +
+  labs(y = "Detached leaf assay") +
+  scale_x_discrete(labels=c("a" = "SB:Dassel", "c" = "DB:Alvorada")) +
+  theme(axis.title.x = element_blank())
+
+p3 <- st %>%
+  ggplot(mapping=aes(x=proj, y = mean)) +
+  geom_jitter(width = .1, height = 0, shape=21, color="black", fill="orange", size=3.5, alpha=1/2) +
+  stat_summary(fun.y=mean, geom="point", shape=95, size=12, color="black") + 
+  theme_minimal() +
+  scale_y_continuous(position = "right") +
+  labs(y = "Straw test rating") +
+  scale_x_discrete(labels=c("b" = "DB:G122", "d" = "DB:Alvorada")) +
+  theme(axis.title.x = element_blank())
+grid.newpage()
+grid.draw(cbind(ggplotGrob(p2), ggplotGrob(p3), size = "last"))
+
+
+
+
 
 ######## need to decide what kind of plot to use and obtain same data for the other isolates
 
+
+### LSD Test --
+
+model2<-aov(Area~Name, data=eproj)
+out2 <- LSD.test(model2,"Name", p.adj="bonferroni")
+plot(out2)
+
+model3 <- aov(`8 dai (cm)`~Isolate, data=bproj)
+out3 <- LSD.test(model3, "Isolate", p.adj="bonferroni")
+plot(out3)
 
 
 #+
